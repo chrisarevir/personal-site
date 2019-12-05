2ff53116f9ceb535c56eda050251f95e4c915ed7
Christian <chris.arevir@gmail.com>
2019-12-04 23:32:06 -0800

    feat: Initial commit and adding in conventional commits

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
 
 const App: React.FC = () => {
   return (
@@ -21,6 +22,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
