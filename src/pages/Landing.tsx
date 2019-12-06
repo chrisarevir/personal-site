09dd44055d5225c00429ab0e89e99d540c730ec2
Christian <chris.arevir@gmail.com>
2019-12-06 00:40:19 -0800

    chore: wip note for animating rotation

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import ProfileCard from '../components/ProfileCard';
 
+// TODO: use react-transition-group to animate the 15 degree rotation for the card
+// NOTE: https://dev.to/terrierscript/styled-component--react-transition-group--very-simple-transition-jja
+
 const Landing: React.FC = () => {
   return <ProfileCard />;
 };
